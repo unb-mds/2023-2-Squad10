37eed639b685b1d08d4ac00514cd9ebff96eac22
Filipe Carvalho <002.f1l1p3@gmail.com>
2023-09-26 22:41:42 -0300

    primeiro esboço da página

---
+++ Projeto/index.js
@@ -0,0 +1,43 @@
+// Funçao para adiconar as Barra na Data e bloqueiar Letras
+const dataInput = document.getElementById('data');
+dataInput.addEventListener('input', (event) => {
+    // Remove caracteres não numéricos
+    const numericValue = event.target.value.replace(/\D/g, '');
+
+    // Insere as barras automaticamente
+    if (numericValue.length >= 2) {
+        const formattedValue = `${numericValue.slice(0, 2)}/${numericValue.slice(2, 4)}/${numericValue.slice(4, 8)}`;
+        dataInput.value = formattedValue;
+    } else {
+        dataInput.value = numericValue;
+    }
+});
+
+//Funçao para Buscar apenas por Numero de Edição
+const edicaoInput = document.getElementById('edicao');
+
+edicaoInput.addEventListener('input', () => {
+    const edicaoValue = edicaoInput.value;
+
+    // Verifica se o valor contém letras
+    if (/[a-zA-Z]/.test(edicaoValue)) {
+        // Contém letras, exibe um alerta
+        alert('Apenas números são permitidos!');
+        edicaoInput.value = ''; // Limpa o campo
+    }
+});
+
+function verificarNumero() {
+    const edicaoValue = edicaoInput.value;
+
+    // Verifica se o valor é um número
+    if (/^\d+$/.test(edicaoValue)) {
+        // É um número, ação desejada
+        console.log('Número válido: ' + edicaoValue);
+    } else {
+        // Não é um número, exibe um alerta (opcional)
+        alert('Apenas números são permitidos!');
+        edicaoInput.value = ''; // Limpa o campo (opcional)
+    }
+}
+
