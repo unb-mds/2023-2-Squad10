9303578ff56a6334961eaa54f22f1b96463d83ba
Carlos42511 <carlocarlo960@gmail.com>
2023-12-01 08:38:38 -0300

    Adicionado o Ano nos arquivos JSON e ATT Separado

--- Projeto/ConversorPDF-JSON/Conversores Python/Separador de Dados.py
+++ Projeto/ConversorPDF-JSON/Conversores Python/Separador de Dados.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 import os
 
 # Caminho do arquivo JSON
-caminho_arquivo = "C:/Users/Carlos/2023-2-Squad10/Projeto/ConversorPDF-JSON/Dados JSON/2023 dados.json"
+caminho_arquivo = "C:/Users/Carlos/2023-2-Squad10/Projeto/ConversorPDF-JSON/Dados JSON/Dados Gerais 2021 a 2023 JSON/2022 dados.json"
 
 # Dicionário padrão para armazenar dados por município
 dados_por_municipio = defaultdict(list)
@@ -11,6 +11,10 @@ dados_por_municipio = defaultdict(list)
 # Ler dados do arquivo JSON
 with open(caminho_arquivo, "r", encoding="utf-8") as file:
     dados_json = json.load(file)
+
+# Adicionar a chave "Ano": "2021" em cada item
+for dado in dados_json:
+    dado["Ano"] = "2023"
 
 # Organizar os dados por município
 for dado in dados_json:
